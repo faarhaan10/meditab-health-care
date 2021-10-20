b38ee057dfeed57316f7dd499c67ac15e0b385fe
Farhan Elias <faarhaan10@gmail.com>
2021-10-20 23:38:48 +0600

    project done

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -6,7 +6,7 @@ const CheckOut = () => {
         <Container>
             <div className="d-flex justify-content-center align-items-center">
                 <div className="my-4">
-                    <h2 className="text-center">Thanks for Purchasing our service</h2>
+                    <h2 className="text-center pt-4">Thanks for Purchasing our service</h2>
                     <Image fluid src="https://lh3.googleusercontent.com/proxy/aGxya_Q7HffcDuFmYCidUkk9hg6YPxxC3ZCUpPSySi5RnwyZyt6JCjxeT1XJQKj-L2580Didy69dl_Q_715YjUxBsnrL0pOalqAPOxOlLIvk-QvT6DpxYpfyhrFiFM5tgj5dl46K6ITlo_dl6F1-qsqJjSQOl9H3FDlcOTA43MovhnQKvb_U_Lql3N0dKh3VMf670qRRwWsZh8yzBv38LYAj82TUB10Z7Mc"/>
                     <h3 className="text-center">Stay Connected, Stay Safe!</h3>
                 </div>
